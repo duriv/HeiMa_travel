6a833f9bb04af4e9307d195e29f899be5e66a704
浮生微雪 <aizijiaoen@gmail.com>
2020-05-19 20:08:44 +0800

    今天完成了黑马旅游网的第一天内容。 存到云端。 明晚将存储第二天的内容。 也有可能把第三天的也给敲完上传

---
+++ src/main/java/cn/itcast/service/UserService.java
@@ -0,0 +1,17 @@
+package cn.itcast.service;
+
+import cn.itcast.bean.User;
+
+public interface UserService {
+    /**
+     * 注册用户
+     * @param user
+     * @return
+     */
+    boolean regist(User user);
+
+
+    boolean active(String code);
+
+    User login(User user);
+}
---
+++ src/main/java/cn/itcast/service/impl/UserServiceImpl.java
@@ -0,0 +1,66 @@
+package cn.itcast.service.impl;
+
+import cn.itcast.bean.User;
+import cn.itcast.dao.UserDao;
+import cn.itcast.dao.impl.UserDaoImpl;
+import cn.itcast.service.UserService;
+import cn.itcast.utils.MailUtils;
+import cn.itcast.utils.UuidUtil;
+
+public class UserServiceImpl implements UserService {
+    private UserDao dao = new UserDaoImpl();
+    /**
+     * 注册用户
+     * @param user
+     * @return
+     */
+    @Override
+    public boolean regist(User user) {
+        //1.根据用户名查询用户信息
+        User u = dao.findByUsername(user.getUsername());
+        //2.保存用户信息
+        //判断用户名是否为空
+        if (u != null){
+            //用户名存在，注册失败
+            return false;
+        }
+        //2.保存用户信息
+        //2.1设置激活码，唯一字符串
+        user.setCode(UuidUtil.getUuid());
+        //2.2设置激活状态
+        user.setStatus("N");
+        dao.save(user);
+        //3.激活邮件发送，邮件正文
+        String content = "<a href='http://localhost/travel/activeUserServlet?code="+user.getCode()+"'>点击激活【 黑马旅游网】</a>";
+        MailUtils.sendMail(user.getEmail(),content,"激活邮件");
+        return true;
+    }
+
+    /**
+     * 激活用户
+     * @param code
+     * @return
+     */
+    @Override
+    public boolean active(String code) {
+        //1.根据激活码查询用户对象
+        User user = dao.findByCode(code);
+        if (user != null){
+            //2.调用dao的修改激活状态的方法
+            dao.updateStatus(user);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    /**
+     * 登陆方法
+     * @param user
+     * @return
+     */
+    @Override
+    public User login(User user) {
+        return dao.findByUsernameAndPassword(user.getUsername(),user.getPassword());
+    }
+}
